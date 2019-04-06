ccf37fc6bbe4c0a4b872b81ab9753b25b35daa5c
Eubaaa <49351832+Eubaaa@users.noreply.github.com>
2019-04-06 19:37:48 -0300

    Add files via upload

---
+++ TrabalhoKalyfe/src/trabalhokalyfe/Livro.java
@@ -0,0 +1,57 @@
+
+
+package trabalhokalyfe;
+
+//classe Livro
+
+public class Livro {
+   // atributos
+    
+    private String nome;
+    private String autor;
+    private int ano;
+    private int edicao;
+    
+// metodos getters e stters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+    
+    // construtor personalizado
+    
+    Livro(){
+      nome ="the book";
+      autor ="Kalyf";
+     ano = 2019;
+     edicao = 3;
+    }
+    
+}
